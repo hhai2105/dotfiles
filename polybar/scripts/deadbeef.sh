ef1e07f430ca6288215241aad77301999aabcc36
Hai Nguyen <hhai2105@gmail.com>
2021-08-07 13:58:00 -0400

    add volume up, down and play pause functionality

--- polybar/scripts/deadbeef.sh
+++ polybar/scripts/deadbeef.sh
@@ -1,13 +1,48 @@
 #!/usr/bin/env zsh
 
-main() {
-    if ! pgrep -x deadbeef-main >/dev/null;
+maxVol=100
+minVol=0
+VolUp(){
+    curvol=$(deadbeef --volume | grep -Po ".{0,3}%" | sed "s/%//g")
+    if [ $curvol -ge $maxVol ]
     then
-	echo "";
 	exit;
     fi
-    OUTPUT=$(deadbeef --nowplaying-tf "%artist% - %title%")
+    deadbeef --volume $(( curvol + 1 ))
+}
+VolDown(){
+    curvol=$(deadbeef --volume | grep -Po ".{0,3}%" | sed "s/%//g")
+    if [ $curvol -le $minVol ]
+    then
+	exit;
+    fi
+    deadbeef --volume $(( curvol - 1 ))
+}
+play-pause(){
+    deadbeef --play-pause
+    
+}
+main() {
+    OUTPUT=$(deadbeef --nowplaying-tf "%artist% - %title% \[ %playback_time% - %length% \]")
     echo "${OUTPUT}"
 }
+if ! pgrep -x deadbeef-main >/dev/null;
+then
+    echo "";
+    exit;
+fi
+case "$1" in
+    --up)
+	VolUp
+	;;
+    --down)
+	VolDown
+	;;
+    --toggle)
+	play-pause
+	;;
+    *)
+	main "$@";
+	;;
+esac
 
-main "$@"
